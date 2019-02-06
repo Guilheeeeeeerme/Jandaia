30ff55a66b90f6f7e4a9b96afd1b08220a842c77
Your Name <you@example.com>
2019-02-06 18:46:09 -0200

    new kaystore

--- build.bat
+++ build.bat
@@ -8,24 +8,26 @@ REM ionic cordova build ios --verbose
 REM ionic cordova run android
 REM exit;
 
-del /q /s Jandaia.keystore
+REM del /q /s Jandaia.keystore
 del /q /s jandaia.apk
 del /q /s ..\jandaia.apk
 
 cls
 echo 'Keytool !!!'
+pause
 
-keytool.exe ^
--genkey -v ^
--keystore Jandaia.keystore ^
--alias Jandaia ^
--keyalg RSA ^
--keysize 2048 ^
--validity 10000 ^
--dname "CN=Guilherme Ferreira, OU=Development, O=Jandaia, L=Itajub , ST=Minas Gerais, C=BR"
+REM keytool.exe ^
+REM -genkey -v ^
+REM -keystore Jandaia.keystore ^
+REM -alias Jandaia ^
+REM -keyalg RSA ^
+REM -keysize 2048 ^
+REM -validity 10000 ^
+REM -dname "CN=Guilherme Ferreira, OU=Development, O=Jandaia, L=Itajub , ST=Minas Gerais, C=BR"
 
 cls
 echo 'jarsigner !!!'
+pause
 
 jarsigner.exe ^
 -verbose ^
@@ -33,10 +35,9 @@ jarsigner.exe ^
 -digestalg SHA1 ^
 -keystore Jandaia.keystore "C:\Users\Guilherme\Dropbox\Jandaia\Jandaia\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk" Jandaia
 
-
 cls
 echo 'zipalign !!!'
+pause
 
 zipalign.exe ^
 -v 4 "C:\Users\Guilherme\Dropbox\Jandaia\Jandaia\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk" ..\jandaia.apk
-
