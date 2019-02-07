e8d0c0f36ac26d085bd209f8f60f3c6f138bb062
Your Name <you@example.com>
2019-02-07 00:11:04 -0200

    done

--- build.bat
+++ build.bat
@@ -2,8 +2,11 @@ set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_201
 SET PATH=%PATH%;C:\Program Files\Java\jdk1.8.0_201\bin
 SET PATH=%PATH%;C:\Users\Guilherme\AppData\Local\Android\Sdk\build-tools\28.0.3
 
-ionic cordova build --release android
-exit
+REM npm install
+REM ionic cordova prepare
+
+REM ionic cordova build --release android
+REM exit
 
 REM ionic cordova build ios --verbose
 
